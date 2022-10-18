a619db706a35dcf5cf47ff477c4cb33fd4e9e11f
Boris Karakolev <boriskarakolev@hotmail.com>
2022-10-18 14:32:36 +0200

    Config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
+const defaultTheme = require("tailwindcss/defaultTheme")
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Open Sans", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        "abn-green-1": "#009286",
+        "abn-green-2": "#005554",
+        "abn-yellow-1": "#FDB912",
+        "black-1": "#141414"
+      },
+    },
   },
   plugins: [],
 }

9fd2fce6dfa14bd1800bb46781d88c7985eba7d6
Boris Karakolev <boriskarakolev@hotmail.com>
2022-10-19 16:00:50 +0200

    Delete store folder

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import TvShows from "@/views/TvShows.vue";
-import ComingSoon from "@/utils/ComingSoon.vue";
+import ComingSoon from "@/views/ComingSoon.vue";
 
 const routes = [
   {

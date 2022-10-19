f424f9b8b2d82749ad3a580c3bb4d94b342d1ccd
Boris Karakolev <boriskarakolev@hotmail.com>
2022-10-19 15:58:51 +0200

    Reduce gap

--- src/components/DashboardList.vue
+++ src/components/DashboardList.vue
@@ -1,6 +1,6 @@
 <template>
   <h1 class="mb-5">{{ title }}</h1>
-  <div class="grid grid-cols-10 gap-5">
+  <div class="grid grid-cols-10 gap-3">
     <div v-for="show in shows" :key="show.id">
       <img :src="show.image.medium" />
     </div>

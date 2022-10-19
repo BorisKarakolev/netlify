4123448ec3620e9c5e8a303abf0342b38f04bb7b
Boris Karakolev <boriskarakolev@hotmail.com>
2022-10-19 15:57:38 +0200

    Add ant and style

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from "vue";
 import App from "@/App.vue";
 import router from "@/router";
-import store from "@/store";
+import Antd from "ant-design-vue";
+// import "ant-design-vue/dist/antd.css";
 import '@/assets/tailwind.css'
 
-createApp(App).use(store).use(router).mount("#app");
+createApp(App).use(router).use(Antd).mount("#app");

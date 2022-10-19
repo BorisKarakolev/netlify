986674084b7f963457ee95961871bbc5da00bb6e
Boris Karakolev <boriskarakolev@hotmail.com>
2022-10-19 15:58:34 +0200

    Change coming soon

---
+++ src/views/ComingSoon.vue
@@ -0,0 +1,14 @@
+<template>
+  <div
+    class="w-full flex flex-col items-center justify-center text-white pt-10"
+  >
+    <img src="@/assets/underconstuction.webp" />
+    <h1 class="text-6xl">Coming soon</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ComingSoon",
+};
+</script>

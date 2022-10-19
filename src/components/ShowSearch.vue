97dc07811a8050631cd79e78047f283479725a93
Boris Karakolev <boriskarakolev@hotmail.com>
2022-10-19 16:00:09 +0200

    Add search input

---
+++ src/components/ShowSearch.vue
@@ -0,0 +1,40 @@
+<template>
+  <form
+    class="flex items-center w-full h-12 border border-solid border-black-1 rounded-3xl p-3 shadow-lg"
+  >
+    <SearchOutlined />
+    <div class="flex flex-nowrap flex-1 h-full text-base text-abn-yellow-1">
+      <div class="relative flex items-center flex-1 h-full p-2">
+        <input
+          type="text"
+          :value="inputText"
+          placeholder="Find your show"
+          class="w-full text-lg focus:outline-none bg-abn-green-2"
+          @input="searchShow($event.target.value)"
+        />
+      </div>
+    </div>
+  </form>
+</template>
+
+<script>
+import { SearchOutlined } from "@ant-design/icons-vue";
+export default {
+  name: "ShowSearch",
+  components: {
+    SearchOutlined,
+  },
+  props: {
+    searchShow: {
+      type: Function,
+      required: true
+    }
+  },
+  data() {
+    return {
+      inputText: '',
+    };
+  },
+  methods: {},
+};
+</script>
--- src/views/TvShows.vue
+++ src/views/TvShows.vue
@@ -1,62 +1,66 @@
 <template>
-  <div class="w-full bg-abn-green-2 text-white p-10">
-    <div
-      v-if="loading"
-      class="w-full h-screen text-6xl flex items-center justify-center"
-    >
-      Loading...
+  <div class="w-full text-white p-10 relative">
+    <h1 class="text-2xl mb-20">TV Shows</h1>
+    <div class="absolute right-10 top-10 w-1/5">
+      <show-search :search-show="searchShow" />
     </div>
-    <div
-      v-if="!shows"
-      class="w-full h-screen text-6xl flex items-center justify-center"
-    >
-      No shows to show
-    </div>
-    <div v-else>
-      <h1 class="text-2xl mb-20">TV Shows</h1>
-      <div class="space-y-5">
-        <dashboard-list
-          :title="'Top 10 rated Tv Shows'"
-          :shows="getTopRated(shows, 10)"
-        />
-        <dashboard-list
-          :title="'Comedies'"
-          :shows="getGenres(shows, 'Comedy')"
-        />
-        <dashboard-list
-          :title="'Actions'"
-          :shows="getGenres(shows, 'Action')"
-        />
-        <dashboard-list :title="'Sports'" :shows="getGenres(shows, 'Sports')" />
-        <dashboard-list
-          :title="'Horrors'"
-          :shows="getGenres(shows, 'Horror')"
-        />
+    <loading-outlined
+      v-if="loading && shows && searchedShows"
+      class="w-full text-6xl flex items-center justify-center"
+    />
+    <empty
+      v-if="!shows && !searchedShows && !searching"
+      class="w-full text-6xl flex items-center justify-center"
+      description="No Shows"
+    />
+    <div v-if="searchedShows && searchedShows.length > 0">
+      <div class="grid grid-cols-10 gap-3">
+        <div v-for="show in searchedShows" :key="show.show.id">
+          <img :src="show.show.image.medium" />
+        </div>
       </div>
+    </div>
+    <div v-if="shows && shows.length > 0" class="space-y-5">
+      <dashboard-list
+        :title="'Top 10 rated Tv Shows'"
+        :shows="getTopRated(shows, 10)"
+      />
+      <dashboard-list :title="'Comedies'" :shows="getGenres(shows, 'Comedy')" />
+      <dashboard-list :title="'Actions'" :shows="getGenres(shows, 'Action')" />
+      <dashboard-list :title="'Sports'" :shows="getGenres(shows, 'Sports')" />
+      <dashboard-list :title="'Horrors'" :shows="getGenres(shows, 'Horror')" />
     </div>
   </div>
 </template>
 
 <script>
 import DashboardList from "@/components/DashboardList.vue";
+import ShowSearch from "@/components/ShowSearch.vue";
 import { getTopRated, getGenres } from "@/utils/helpers";
+import { Empty } from "ant-design-vue";
+import { LoadingOutlined } from "@ant-design/icons-vue";
 export default {
   name: "TvShows",
   components: {
     DashboardList,
+    ShowSearch,
+    LoadingOutlined,
+    Empty,
   },
   data() {
     return {
       loading: true,
+      searching: true,
       shows: null,
+      searchedShows: null,
     };
   },
   mounted() {
-    this.fetchData();
+    this.fetchShows();
   },
   methods: {
-    fetchData() {
-      this.post = null;
+    fetchShows() {
+      this.shows = null;
       this.loading = true;
       fetch("https://api.tvmaze.com/shows")
         .then((res) => res.json())
@@ -66,8 +70,26 @@ export default {
           console.log(data);
         })
         .catch((err) => {
+          this.loading = false;
           console.log(err);
         });
+    },
+    searchShow(inputText) {
+      this.searchedShows = null;
+      this.searching = true;
+      setTimeout(() => {
+        fetch(`https://api.tvmaze.com/search/shows?q=${inputText}`)
+          .then((res) => res.json())
+          .then((data) => {
+            this.searching = false;
+            this.searchedShows = data;
+            this.shows = null;
+          })
+          .catch((err) => {
+            this.searching = false;
+            console.log(err);
+          });
+      }, 500);
     },
     getTopRated,
     getGenres,

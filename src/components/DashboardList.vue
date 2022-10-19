d93d4bb4b23d64cb700910af169a4e25fbf3adea
Boris Karakolev <boriskarakolev@hotmail.com>
2022-10-19 02:26:43 +0200

    Add dashboard list

---
+++ src/components/DashboardList.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1 class="mb-5">{{ title }}</h1>
+  <div class="grid grid-cols-10 gap-5">
+    <div v-for="show in shows" :key="show.id">
+      <img :src="show.image.medium" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DashboardList",
+  props: {
+    shows: { type: Array, required: true },
+    title: { type: String, required: true },
+  },
+};
+</script>

2278c04d0e88ce7acfffcdc1b170c286fb6bc977
Boris Karakolev <boriskarakolev@hotmail.com>
2022-10-20 13:20:33 +0200

    Create image-modal comp

--- src/components/DashboardList.vue
+++ src/components/DashboardList.vue
@@ -2,14 +2,18 @@
   <h1 class="mb-5">{{ title }}</h1>
   <div class="grid grid-cols-10 gap-3">
     <div v-for="show in shows" :key="show.id">
-      <img :src="show.image.medium" />
+      <image-modal :show="show" />
     </div>
   </div>
 </template>
 
 <script>
+import ImageModal from "@/components/Image.vue";
 export default {
   name: "DashboardList",
+  components: {
+    ImageModal,
+  },
   props: {
     shows: { type: Array, required: true },
     title: { type: String, required: true },
---
+++ src/components/Image.vue
@@ -0,0 +1,30 @@
+<template>
+  <img :src="show.image?.medium" @click="toggleModal" />
+  <modal :close="toggleModal" :modal-open="modalOpen" :show="show" />
+</template>
+
+<script>
+import Modal from "@/components/Modal.vue";
+import { ref } from "vue";
+export default {
+  name: "ImageModal",
+  components: {
+    Modal,
+  },
+  props: {
+    show: {
+      type: Object,
+      required: true,
+    },
+  },
+  setup() {
+    const modalOpen = ref(false);
+
+    const toggleModal = () => {
+      modalOpen.value = !modalOpen.value
+    }
+
+    return { modalOpen, toggleModal }
+  },
+};
+</script>
